531edca43612c576b7b5e321bab6782c94969ce9
enerGneer <energneer@gmail.com>
2022-10-19 23:37:25 +0900

    rollback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ import posthog from 'posthog-js'
 import 'react-notion-x/src/styles.css'
 import 'styles/global.css'
 // this might be better for dark mode
- import 'prismjs/themes/prism-okaidia.css'
+// import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
