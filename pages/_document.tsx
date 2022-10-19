3367055a21aa2a74aed22312eebdfa1642da1d84
enerGneer <energneer@gmail.com>
2022-10-19 21:59:39 +0900

    font change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,9 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
-            <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css' />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet" />
           </Head>
 
           <body>
